b2ae097155e225c3eb67e928e4978c1d6e730049
zero <783352994@qq.com>
2015-04-16 17:12:51 +0800

    加入webView

--- app/src/main/java/com/zero/hkdnews/ui/NewsActivity.java
+++ app/src/main/java/com/zero/hkdnews/ui/NewsActivity.java
@@ -1,56 +1,161 @@
 package com.zero.hkdnews.ui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.text.Html;
-import android.text.Spanned;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
-import android.widget.ImageButton;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewSwitcher;
 
 import com.zero.hkdnews.R;
+import com.zero.hkdnews.beans.News;
+import com.zero.hkdnews.common.UIHelper;
 
 import cn.sharesdk.framework.ShareSDK;
 import cn.sharesdk.onekeyshare.OnekeyShare;
-import uk.co.deanwild.flowtextview.FlowTextView;
 
 /**
+ * 新闻详情
  * Created by luowei on 15/4/11.
  */
-public class NewsActivity extends Activity implements View.OnClickListener{
+public class NewsActivity extends Activity{
 
-    private ImageButton share_imgBtn;
-    private ImageButton comment_imgBtn;
-    private ImageButton comList_imgBtn;
+    private Context context ;
 
-     private FlowTextView ftv;
+    private LinearLayout mHeader; //上面的头部导航
+
+    private LinearLayout mFooter; //底部的导航
+    private ImageView mHome;  //头部的home
+    private ImageView mRefresh;
+    private TextView mHeadTitle;
+    private ScrollView mScrollView;
+    private ViewSwitcher mViewSwitcher;
+
+    private ImageView mComment;
+    private ImageView mDetail;
+    private ImageView mCommentList;
+    private ImageView mShare;
+
+    private WebView mWebView;
+    private Handler mHandler;
+
+    private News newsDetail;
+    private String newsId;
+
+    private ViewSwitcher mFootViewSwitcher;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_news);
-
+        setContentView(R.layout.activity_news2);
 
         initView();
-
-        share_imgBtn.setOnClickListener(this);
-        comList_imgBtn.setOnClickListener(this);
-        comment_imgBtn.setOnClickListener(this);
-
-
-
-        Spanned html = Html.fromHtml("<h1>welcome to mylife <h1>");
-
-        ftv.setText(html);
+        initData();
 
 
     }
 
+    private void initData() {
+        mHandler = new Handler() {
+            public void handleMessage(Message msg){
+                if(msg.what == 1){
+                    String body = (String) msg.obj;
+                    mWebView.loadDataWithBaseURL(null,body,"text/html","utf-8",null);
+                    mWebView.setWebViewClient(new WebViewClient(){
+                        @Override
+                        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                            view.loadUrl(url);
+                            return super.shouldOverrideUrlLoading(view,url);
+                        }
+                    });
+
+                }
+            }
+        };
+
+        initDate(newsId,false);
+
+    }
+
+
+    private void initDate(final String newsId,final boolean isRefresh){
+        new Thread(){
+            public void run(){
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                //要显示的页面
+                String html = "<ul>\n" +
+                        "  <li><em>学历</em> 由于拿到offer后，办理工作签证时出示本科成绩单。所以本科学历是最低要求的。</li>\n" +
+                        "  <li><em>英语</em> 英语没有硬性要求，不需要雅思托福成绩。个人觉得能无障碍的听懂youtube上的技术分享，会一些基本日常语法加上相关专业词汇，就能比较顺利的完成电面和人肉面。</li>\n" +
+                        "  <li><em>专业经验</em> 没有硬性的相关领域证书要求，当然如果你没有内推渠道，有个把证可以增加通过简历过滤器脱引而出的机会。</li>\n" +
+                        "  <li><em>技术经验</em> 是否有能力维护设计Facebook服务器量级的系统是一个重要考察点。当然不要求你一定要经历过这么大的量级经验（毕竟这样的公司不多）。</li>\n" +
+                        "  <li><em>家庭</em> “一人Offer，全家受益”是我对Facebook Relocation的总结。拿到Offer后的所有环节，Facebook都会把你的家庭（配偶和子女）作为一个整体考虑进去。所以只要家人支持，家庭不会成为入职的羁绊。</li>\n" +
+                        "  <li><em>国外生活经历</em> 博主在去Facebook前，除了一次自助蜜月游，从来没有出过国。也证明这方面没有硬性要求。个人觉得生活就像学游泳，扔进水里了，扑腾几下怎么样都会了。</li>\n" +
+                        "  <li><em>会翻墙</em> 呵呵。。。</li>\n" +
+                        "</ul>";
+
+                Message msg = new Message();
+                msg.what = 1;
+                msg.obj = html;
+                mHandler.sendMessage(msg);
+            }
+
+        }.start();
+    }
+
     private void initView() {
-        ftv = (FlowTextView) findViewById(R.id.ftv);
-        share_imgBtn = (ImageButton) findViewById(R.id.news_bottom_share);
-        comment_imgBtn = (ImageButton) findViewById(R.id.news_bottom_comment);
-        comList_imgBtn = (ImageButton) findViewById(R.id.news_bottom_comment_list);
+
+        context =this;
+        newsId = getIntent().getStringExtra("news_id");
+
+        mHeader = (LinearLayout) findViewById(R.id.news_header);
+        mFooter = (LinearLayout) findViewById(R.id.news_detail_footer);
+        mHome = (ImageView) findViewById(R.id.news_header_home);
+        mRefresh = (ImageView) findViewById(R.id.news_header_refresh);
+        mHeadTitle = (TextView) findViewById(R.id.news_header_title);
+
+        mViewSwitcher = (ViewSwitcher) findViewById(R.id.news_detail_viewswitcher);
+        mScrollView = (ScrollView) findViewById(R.id.news_detail_scrollview);
+
+        mComment = (ImageView) findViewById(R.id.news_detail_foot_com);
+        mDetail = (ImageView) findViewById(R.id.news_detail_foot_detail);
+        mCommentList  = (ImageView) findViewById(R.id.news_detail_foot_comlist);
+        mShare = (ImageView) findViewById(R.id.news_detail_foot_share);
+
+        mDetail.setEnabled(false);
+
+        mWebView = (WebView) findViewById(R.id.news_detail_webview);
+        mWebView.getSettings().setSupportZoom(true);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.getSettings().setDefaultFontSize(15);
+
+
+   //     UIHelper.addWebImageShow(this,mWebView);
+
+        mHome.setOnClickListener(homeClickListenter);
+        mRefresh.setOnClickListener(refreshClickListener);
+        mComment.setOnClickListener(comClickListener);
+        mDetail.setOnClickListener(detailClickListener);
+        mCommentList.setOnClickListener(comListClickListener);
+        mShare.setOnClickListener(shareClickListener);
+
+
+
     }
 
     private void showShare(){
@@ -65,22 +170,55 @@ public class NewsActivity extends Activity implements View.OnClickListener{
         oks.show(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            //点击分享事件
-            case R.id.news_bottom_share:
-                showShare();
-              //  Toast.makeText(this,"you click share",Toast.LENGTH_SHORT).show();
-                break;
-            //评论事件
-            case R.id.news_bottom_comment:
-                Toast.makeText(this,"you click comment",Toast.LENGTH_SHORT).show();
-                break;
-            //进入评论列表
-            case R.id.news_bottom_comment_list:
-                Toast.makeText(this,"you click list",Toast.LENGTH_SHORT).show();
-                break;
+    private View.OnClickListener homeClickListenter = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            UIHelper.showHome(context);
+            finish();
+           // Toast.makeText(context,"Click Home!",Toast.LENGTH_SHORT).show();
         }
-    }
+    };
+
+    private View.OnClickListener refreshClickListener = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            initDate(newsId,true);
+            Toast.makeText(context,"refresh",Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private View.OnClickListener shareClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(context,"share",Toast.LENGTH_SHORT).show();
+            showShare();
+        }
+    };
+
+    private View.OnClickListener detailClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+        Toast.makeText(context,"detail",Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private View.OnClickListener comListClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+        Toast.makeText(context,"comList",Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private View.OnClickListener comClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+        Toast.makeText(context,"comment",Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
+
+
 }
