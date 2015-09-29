ea8e7a98d6c1f1e73a6e591c9e5b148e26db26d2
zero <783352994@qq.com>
2015-09-29 23:37:24 +0800

    优化通知

--- app/src/main/java/com/zero/hkdnews/fragment/InformFragment.java
+++ app/src/main/java/com/zero/hkdnews/fragment/InformFragment.java
@@ -1,6 +1,7 @@
 package com.zero.hkdnews.fragment;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -16,10 +17,12 @@ import com.zero.hkdnews.activity.GroupActivity;
 import com.zero.hkdnews.adapter.PlayAdapter;
 import com.zero.hkdnews.beans.HnustUser;
 import com.zero.hkdnews.beans.Inform;
+import com.zero.hkdnews.util.T;
 
 import java.util.ArrayList;
 import java.util.List;
 import android.os.Handler;
+import android.widget.TextView;
 
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.BmobUser;
@@ -32,25 +35,35 @@ import cn.bmob.v3.listener.FindListener;
  * Created by zero on 15/4/11.
  */
 public class InformFragment extends Fragment {
+    private static final String TAG = "InformFragment";
 
-    private PlayAdapter adapter;
-    private ListView listView;
-    private List<Inform> datas;
-
-    private FloatingActionButton fab;
-
-    private ProgressWheel pw;
+    private static final int QUERY_SUCCESS = 1;
+    private static final int QUERY_FAILED = 2;
 
 
-    private Handler mHandler = new Handler(){
+    private PlayAdapter mAdapter;
+    private ListView mInformLv;
+    private List<Inform> mData = new ArrayList<>();
+
+    private FloatingActionButton mFAB;
+
+    private ProgressWheel mLoadPw;
+    private TextView mEmptyTv;
+
+    private final Handler mHandler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            if (msg.what == 11){
-                datas = (List<Inform>) msg.obj;
-                adapter.setList(datas);
-                adapter.notifyDataSetChanged();
-                pw.setVisibility(View.GONE);
+            if (msg.what == QUERY_SUCCESS) {
+                mData = (List<Inform>) msg.obj;
+                mAdapter.setList(mData);
+                mAdapter.notifyDataSetChanged();
+                showEmptyText(false);
+
+            } else if (msg.what == QUERY_FAILED) {
+                String log = (String) msg.obj;
+                T.showShortBar(mFAB, log);
+                showEmptyText(true);
             }
         }
     };
@@ -67,15 +80,12 @@ public class InformFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         initView();
+        queryData();
 
-        initDatas();
+        mAdapter = new PlayAdapter(mData, getActivity());
+        mInformLv.setAdapter(mAdapter);
 
-        addData();
-
-        adapter = new PlayAdapter(datas,getActivity());
-        listView.setAdapter(adapter);
-
-        fab.setOnClickListener(new View.OnClickListener() {
+        mFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), GroupActivity.class);
@@ -84,30 +94,18 @@ public class InformFragment extends Fragment {
         });
     }
 
-    private void initDatas() {
-        datas = new ArrayList<>();
-//        Inform inform1 = new Inform("来自－IT2班","请注意，体育检测推迟到6月14日！");
-//        Inform inform2 = new Inform("来自－ELAA社团","本周五将在北校体育馆开展英语角！");
-//
-//        datas.add(inform1);
-//        datas.add(inform2);
-
-    }
-
     private void initView() {
-        pw = (ProgressWheel) getActivity().findViewById(R.id.fragment_play_pb);
-
-        listView = (ListView) getActivity().findViewById(R.id.play_list_view);
-        fab = (FloatingActionButton) getActivity().findViewById(R.id.play_fab);
-
-        fab.attachToListView(listView);
+        mLoadPw = (ProgressWheel) getActivity().findViewById(R.id.fragment_play_pb);
+        mInformLv = (ListView) getActivity().findViewById(R.id.play_list_view);
+        mFAB = (FloatingActionButton) getActivity().findViewById(R.id.play_fab);
+        mEmptyTv = (TextView) getActivity().findViewById(R.id.inform_empty_tv);
+//        mFAB.attachToListView(mInformLv);
     }
-
 
     /**
      * 第一次查询数据
      */
-    private void addData(){
+    private void queryData(){
         BmobQuery<Inform> query = new BmobQuery<>();
         HnustUser user = BmobUser.getCurrentUser(getActivity(),HnustUser.class);
         query.addWhereRelatedTo("informs",new BmobPointer(user));
@@ -117,13 +115,28 @@ public class InformFragment extends Fragment {
             public void onSuccess(List<Inform> list) {
                 Message msg = Message.obtain();
                 msg.obj =list;
-                msg.what =11;
+                msg.what = QUERY_SUCCESS;
                 mHandler.sendMessage(msg);
             }
             @Override
             public void onError(int i, String s) {
-
+                Message msg = Message.obtain();
+                msg.obj = s;
+                msg.what = QUERY_FAILED;
+                mHandler.sendMessage(msg);
             }
         });
+    }
+
+    private void showEmptyText(boolean isShow){
+        if (isShow) {
+            mLoadPw.setVisibility(View.GONE);
+            mFAB.setVisibility(View.GONE);
+            mEmptyTv.setVisibility(View.VISIBLE);
+        } else {
+            mLoadPw.setVisibility(View.GONE);
+            mFAB.setVisibility(View.VISIBLE);
+            mEmptyTv.setVisibility(View.GONE);
+        }
     }
 }
